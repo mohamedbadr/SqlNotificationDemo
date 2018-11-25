dfeeb6a91738b68ddadffc3c6ac56459da5c326c
Mohamed Badr <eng.ma.badr@gmail.com>
2018-11-25 09:13:27 +0300

    show how it works with two separate forms

---
+++ NotificationsDemo/FrmReciever.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace NotificationsDemo
+{
+    partial class FrmReciever
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtCount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtCount
+            // 
+            this.TxtCount.Enabled = false;
+            this.TxtCount.Location = new System.Drawing.Point(77, 106);
+            this.TxtCount.Name = "TxtCount";
+            this.TxtCount.Size = new System.Drawing.Size(292, 20);
+            this.TxtCount.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Items Count";
+            // 
+            // FrmReciever
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 217);
+            this.Controls.Add(this.TxtCount);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmReciever";
+            this.Text = "FrmReciever";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmReciever_FormClosing);
+            this.Load += new System.EventHandler(this.FrmReciever_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtCount;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ NotificationsDemo/FrmReciever.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NotificationsDemo.Data;
+using TableDependency.SqlClient;
+using TableDependency.SqlClient.Base.Enums;
+using TableDependency.SqlClient.Base.EventArgs;
+
+namespace NotificationsDemo
+{
+    public partial class FrmReciever : Form
+    {
+        private SqlTableDependency<Item> _dependency;
+
+        public FrmReciever()
+        {
+            InitializeComponent();
+           // _sqlDependency = new SqlDependency();
+          
+        }
+
+        private void FrmReciever_Load(object sender, EventArgs e)
+        {
+            _dependency = new SqlTableDependency<Item>(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString, "Items");
+           // SqlDependency.dependency.Activ
+            _dependency.OnChanged += OnChanged;
+            _dependency.Start();
+        }
+
+        private void FrmReciever_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _dependency.Stop();
+        }
+
+        public void OnChanged(object sender, RecordChangedEventArgs<Item> e)
+        {
+            if (e.ChangeType == ChangeType.Insert)
+            {
+                LoadData();
+            }
+        }
+
+        private void LoadData()
+        {
+            using (var db = new NotificationsDemoEntities())
+            {
+                var items = db.Items.Count();
+                TxtCount.Text = items.ToString();
+            }
+        }
+    }
+}
